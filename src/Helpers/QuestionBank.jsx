04a8b8a6b458eb2d4ece102f3aaab49b00aae556
Ovsclassic <okunoyevictor@gmail.com>
2023-03-15 15:52:34 +0100

    commit

---
+++ src/Helpers/QuestionBank.jsx
@@ -0,0 +1,34 @@
+export const Questions = [
+  {
+    prompt: "Which is a state in Nigeria?",
+    optionA: "Cairo",
+    optionB: "Niger",
+    optionC: "Benue",
+    optionD: "Ghana",
+    answer: "C",
+  },
+  {
+    prompt: "Where is Egypt located?",
+    optionA: "Europe",
+    optionB: "Africa",
+    optionC: "Asia",
+    optionD: "South Sudan",
+    answer: "B",
+  },
+  {
+    prompt: "Best university in the world?",
+    optionA: "Covenant",
+    optionB: "Babcock",
+    optionC: "Unilag",
+    optionD: "Harvard",
+    answer: "D",
+  },
+  {
+    prompt: "What is 2+2-1?",
+    optionA: "1",
+    optionB: "2",
+    optionC: "3",
+    optionD: "4",
+    answer: "C",
+  },
+];
